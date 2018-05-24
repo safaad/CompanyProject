3a0f0e7258eee2063d3c52c54fa44d53e40f818f
Safaa Dyab <33197552+safaad@users.noreply.github.com>
2018-05-24 23:43:48 +0300

    Initial commit

---
+++ src/Individuals/Employee.java
@@ -0,0 +1,16 @@
+package Individuals;
+
+public class Employee extends Person {
+		protected double salary=1000;
+		protected String username;
+		protected boolean admin;
+		protected String password=null;
+		Employee(String first,String last,int a){
+			fn=first;
+			ln=last;
+			age=a;
+			username="" + fn.charAt(0) + fn.charAt(1) +"." + ln.charAt(0).
+		}
+		
+		
+}
