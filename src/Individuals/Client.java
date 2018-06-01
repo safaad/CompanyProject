484a37a2b5e6595fdf09a3c931df3287dea73338
Ibrahim Owaidat <ibrahim.owaidat@outlook.com>
2018-06-02 02:20:19 +0300

    Update Client.java
    nice Client.java

--- src/Individuals/Client.java
+++ src/Individuals/Client.java
@@ -1,12 +1,50 @@
 package Individuals;
 
-public class Client extends Person {
+import java.util.ArrayList;
+import Driver.*;
+import Products.*;
 
-	public Client(String first, String last, int a) {
-		super(first, last, a);
-		// TODO Auto-generated constructor stub
+public class Client extends Person {
+	protected String Username;
+	protected double contribution;
+	protected Employee FavoriteEmployee;
+	protected ArrayList<Product> boughtProducts;
+	//protected ArrayList<Product> Cart;
+
+	public Client(String first, String last, int[] birthday, String username, double total, String EmpName) {
+		super(first, last, birthday);
+		Username = username;
+		contribution = total;
+		boughtProducts = new ArrayList<Product>();
+		//Cart = new ArrayList<Product>();
+		FavoriteEmployee = Menu.Website.getEmployee(EmpName);
+	}
+
+	public String getUsername() {
+		return Username;
 	}
 	
+	public void setUsername(String username) {
+		Username = username;
+	}
+
+	public double getTotal() {
+		return contribution;
+	}
 	
+	private void setTotal(double value) {
+		contribution = value;
+	}
+
+	private Employee getFavEmp() {
+		return FavoriteEmployee;
+	}
 	
+	private void setFavEmp(Employee fav) {
+		FavoriteEmployee = fav;
+	}
+	
+	private void setFavEmp(String favEmpName) {
+		FavoriteEmployee = Menu.Website.getEmployee(favEmpName);
+	}
 }
