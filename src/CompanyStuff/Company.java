eb1db83bae68443fc6e71855f2c9b121a0939bd3
Ibrahim Owaidat <ibrahim.owaidat@outlook.com>
2018-06-02 15:19:05 +0300

    Update Company.java
    Added 2 new methods (might be useful)

--- src/CompanyStuff/Company.java
+++ src/CompanyStuff/Company.java
@@ -1,26 +1,42 @@
 package CompanyStuff;
 
-
+import Individuals.*;
+import Products.*;
 import java.util.ArrayList;
-
-import Individuals.Employee;
 
 public class Company {
 	public ArrayList<Employee> HE = new ArrayList<Employee>();
-	//protected ArrayList<Client> Consumer = new ArrayList<Client>();
-	
-	public boolean exist(String user) {
-		for(Employee e : HE)
-			if(e.getUsername().equals(user))
-				return true;
-		return false;
-	}
-	
+	public ArrayList<Client> Consumer = new ArrayList<Client>();
+	public ArrayList<Product> Products = new ArrayList<Product>();
+
 	public Employee getEmployee(String user) {
-		for(Employee e : HE)
-			if(e.getUsername().equals(user))
+		for (Employee e : HE)
+			if (e.getUsername().equals(user))
 				return e;
 		return null;
 	}
-	//Arraylist of products
+
+	public boolean exist(String user) {
+		for (int i = 0; i < HE.size(); i++)
+			if (HE.get(i).getUsername().equals(user))
+				return true;
+		return false;
+	}
+
+	public boolean isProductExists(Product p) {
+		for (Product p2 : Products) {
+			if (p2.equals(p))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean deleteProduct(Product p) {
+		if (this.isProductExists(p)) {
+			this.Products.remove(p);
+			return true;
+		}
+		return false;
+	}
+
 }
