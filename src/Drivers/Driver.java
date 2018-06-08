ccd6321112f928b06958730dbe48ae45bc5e149b
Ibrahim Owaidat <ibrahim.owaidat@outlook.com>
2018-06-08 22:32:53 +0300

    Update Driver.java

--- src/Drivers/Driver.java
+++ src/Drivers/Driver.java
@@ -2,6 +2,9 @@
 package Drivers;
 
 import java.util.Scanner;
+import java.util.regex.PatternSyntaxException;
+
+import org.omg.Messaging.SyncScopeHelper;
 
 import CompanyStuff.Company;
 import CompanyStuff.savePerson;
@@ -11,14 +14,17 @@ public class Driver {
 	public static Scanner scan = new Scanner(System.in);
 	public static Company Website = new Company(50000);
 	public static savePerson SP = new savePerson();
+
 	public static void clientLogin() {
 
 	}
-	public static void  AdminLogin() {
+
+	public static void AdminLogin() {
 		int choice;
-		boolean login,repeat;
-		
+		boolean login, repeat;
+
 	}
+
 	public static void EmployeeLogin() {
 		int choice = 0, type = 0;
 		int[] a = new int[3];
@@ -49,16 +55,16 @@ public class Driver {
 						login = true;
 				}
 			}
-		
+
 			if (login) {
-				System.out.println("\n\nHello "+e.getUsername()+"\n                       ***Your Menu is***                        \n");
+				System.out.println("\n\nHello " + e.getUsername() + "!\n\t\t\t*** Menu ***\t\t\t\n");
 				repeat1 = true;
 				while (repeat1) {
-					System.out.println("1-Register In:\n2-Register Out");
-					System.out.println(
-							"3-change password\n4-get extra payment\n5-get list attendance\n6-get list of extra attendance");
-					System.out.println("7-add Products to stock");
-					System.out.println("8-get username\n9-Back to menu\n10-Exit from Employee side");
+					System.out.println("1-\tRegister In" + "\n2-\tRegister Out");
+					System.out.println("3-\tChange password" + "\n4-\tGet extra payment");
+					System.out.println("5-\tGet list attendance" + "\n6-\tGet list of extra attendance");
+					System.out.println("7-\tAdd Products to stock" + "\n8-\tGet username");
+					System.out.println("9-\tBack to menu" + "\n10-\tExit from Employee side");
 					choice = scan.nextInt();
 					switch (choice) {
 					case 1:
@@ -69,7 +75,7 @@ public class Driver {
 						break;
 
 					case 3:
-						System.out.println("Enter new password");
+						System.out.print("Enter new password: ");
 						scan.nextLine();
 						String s = scan.nextLine();
 						e.setPassword(s);
@@ -83,18 +89,19 @@ public class Driver {
 						e.PrintAttendanceExtra();
 						break;
 					case 7:
-						System.out.println("enter the product Name and quantity:");
-						String n=scan.nextLine();
+						System.out.print("Enter the product Name and quantity: ");
 						scan.nextLine();
-						int i=scan.nextInt();
-						EmpD.addToStock(n,i);
+						String n = scan.nextLine();
+						int i = scan.nextInt();
+						EmpD.addToStock(n, i);
 						Website.getProduct(n).setItemName(n);
-						System.out.println("successfully added\n------------\n");
+						System.out.println("Successfully added\n------------");
 						Website.PrintListOfProducts();
 						break;
 					case 8:
-						System.out.println(
-								"\n----------------\n" + "username is: " + e.getUsername() + "\n----------------\n");
+						System.out
+								.println("\n----------------\nUsername is: " + e.getUsername() + "\n----------------");
+						break;
 					case 9:
 						login = false;
 						repeat1 = false;
@@ -111,6 +118,42 @@ public class Driver {
 		}
 	}
 
+	public static void ClientLogin() {
+		int choice;
+		Client guest = null;
+		ClientDriver CD = new ClientDriver();
+		boolean loggedIn = false;
+		System.out.println("(1) Login\t(2) Register");
+		scan.nextLine();
+		choice = scan.nextInt();
+		while(!loggedIn) {
+			if (choice == 1) {
+				guest = CD.signIn();
+				loggedIn = true;
+			}
+			if(choice == 2) {
+				guest = CD.signUp();
+				Website.Clients.add(guest);
+				loggedIn = true;
+			}
+			if(choice != 1 && choice != 2)
+				System.out.println("Your choice was none of the above!");
+		}
+		while(loggedIn) {
+			CD.printMenu();
+			choice = scan.nextInt();
+			switch(choice) {
+				case 0:
+					loggedIn = false;
+					break;
+				case 1:
+					CD.viewWishList(guest);
+					break;
+					
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("** Hello and Welcome! **");
 		int choice;
@@ -118,7 +161,7 @@ public class Driver {
 		boolean running = true;
 		SP.read();
 		while (running) {
-			System.out.print("Login as a\n(1) Client\t(2) Employee\n(3) Company Admin");
+			System.out.println("Login as a\n(1) Client\t(2) Employee\n(3) Company Admin");
 			choice = scan.nextInt();
 
 			switch (choice) {
@@ -126,13 +169,13 @@ public class Driver {
 				running = false;
 				break;
 			case 1:
-				AdminLogin();
+				ClientLogin();
 				break;
 			case 2:
 				EmployeeLogin();
 				break;
 			case 3:
-
+				AdminLogin();
 				break;
 			}
 		}
